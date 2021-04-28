7faabee9b94a203229d92a77120d3bffa5e4a73f
Stepan Usiantsev <stepan.usiantsev@saritasa.com>
2021-04-28 10:32:17 +0500

    Initial Commit

---
+++ DownSwift/DownSwift.h
@@ -0,0 +1,18 @@
+//
+//  DownSwift.h
+//  DownSwift
+//
+//  Created by Stepan on 28.04.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DownSwift.
+FOUNDATION_EXPORT double DownSwiftVersionNumber;
+
+//! Project version string for DownSwift.
+FOUNDATION_EXPORT const unsigned char DownSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DownSwift/PublicHeader.h>
+
+
