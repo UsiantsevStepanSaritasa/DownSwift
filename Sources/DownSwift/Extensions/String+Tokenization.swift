2ce0f6133dabe200fcf65470ad2f5dd93488c84b
Stepan Usiantsev <stepan.usiantsev@saritasa.com>
2021-04-30 13:24:44 +0500

    Refactor tokenize algorithm. Imporved its logic

--- Sources/DownSwift/Extensions/String+Tokenization.swift
+++ Sources/DownSwift/Extensions/String+Tokenization.swift
@@ -12,32 +12,21 @@ extension String {
     var textAreas: [Text] {
         /**
          `Text` type optional property that stores current `Text`'s entity state.
-         
-         It's used for *bold*, *italic* and *strikethrough* text styles.
-         
-         - Warning
-                For *regular* text style use `regularPartialText` property!
          */
         var partialText: Text?
-        
-        /**
-         `Text` type optional property that stores current `Text`'s entity state for regular text style.
-         
-         It's used only for *regular* text style.
-         
-         - Warning
-                For *bold*, *italic* and *strikethrough* text styles use `partialText` property!
-         */
-        var regularPartialText: Text?
         
         /// A property that stores an array of `Text` entities.
         var textParts = [Text]()
         
-        /// A boolean property that indicates if character is tokenizing at current moment.
-        var isTokenizing = false
-        
         /// A boolean indicating if current character will be as a symbol within current text area
         var isSkipped = false
+        
+        /// A counter indicating if text area's open or closed.
+        ///
+        /// `areaCounter = 1` - text area is open;
+        ///
+        /// `areaCounter = 2` - text area is closed
+        var areaCounter = 0
         
         /**
             Defines text area depending on character and then either add it to the existing text area or create new one.
@@ -66,9 +55,9 @@ extension String {
                             return
                         }
                             
+                        areaCounter += 1
                         textParts.append(symbol)
-                        isTokenizing = false
-                    } else if isTokenizing {
+                    } else {
                         if character == TextStyle.symbol.rawValue, !isSkipped {
                             isSkipped = true
                             
@@ -80,72 +69,24 @@ extension String {
                             
                             return
                         }
-
-                        symbol.string.append(character)
-                        return
                     }
                     
                     partialText = nil
+                    
+                    if areaCounter < 2 {
+                        partialText = newTextArea(character)
+                    } else {
+                        areaCounter = 0
+                    }
+                    
                     return
                 }
-                
-                symbol.string.append(character)
-                isSkipped = false
-                partialText = symbol
-            } else if var regularSymbol = regularPartialText {
-                if
-                    character != TextStyle.bold.rawValue,
-                    character != TextStyle.italic.rawValue,
-                    character != TextStyle.strikethrough.rawValue
-                {
-                    if character == TextStyle.symbol.rawValue, !isSkipped {
-                        isSkipped = true
-                        
-                        return
-                    }
 
-                    regularSymbol.string.append(character)
-                    regularPartialText = regularSymbol
-                    isSkipped = false
-                } else if isSkipped {
-                    regularSymbol.string.append(character)
-                    regularPartialText = regularSymbol
-                    isSkipped = false
-                } else {
-                    regularPartialText = nil
-                    textParts.append(regularSymbol)
-                    switch character {
-                    case TextStyle.bold.rawValue:
-                        partialText = Text(textStyle: TextStyle.bold, string: "")
-                        isTokenizing = true
-                    case TextStyle.italic.rawValue:
-                        partialText = Text(textStyle: TextStyle.italic, string: "")
-                        isTokenizing = true
-                    case TextStyle.strikethrough.rawValue:
-                        partialText = Text(textStyle: TextStyle.strikethrough, string: "")
-                        isTokenizing = true
-                    default:
-                        break
-                    }
-                }
-            } else if !isTokenizing {
-                /* If character is not tokenizing at the moment and we meet special symbols
-                 then we define what specific zone of markdown we should parse.
-                 We're creating an empty Text entity, where we'll store our markdown zone.
-                 */
-                switch character {
-                case TextStyle.bold.rawValue:
-                    partialText = Text(textStyle: TextStyle.bold, string: "")
-                    isTokenizing = true
-                case TextStyle.italic.rawValue:
-                    partialText = Text(textStyle: TextStyle.italic, string: "")
-                    isTokenizing = true
-                case TextStyle.strikethrough.rawValue:
-                    partialText = Text(textStyle: TextStyle.strikethrough, string: "")
-                    isTokenizing = true
-                default:
-                    regularPartialText = Text(textStyle: TextStyle.regular, string: "\(character)")
-                }
+                symbol.string.append(character)
+                partialText = symbol
+                isSkipped = false
+            } else {
+                partialText = newTextArea(character)
             }
         }
         
@@ -153,10 +94,25 @@ extension String {
         
         if let lastSymbol = partialText, !lastSymbol.string.isEmpty {
             textParts.append(lastSymbol)
-        } else if let lastRegularSymbol = regularPartialText, !lastRegularSymbol.string.isEmpty {
-            textParts.append(lastRegularSymbol)
         }
         
         return textParts
+    }
+}
+
+private func newTextArea(_ character: Character) -> Text {
+    /* If character is not tokenizing at the moment and we meet special symbols
+     then we define what specific zone of markdown we should parse.
+     We're creating an empty Text entity, where we'll store our markdown zone.
+     */
+    switch character {
+    case TextStyle.bold.rawValue:
+        return Text(textStyle: TextStyle.bold, string: "")
+    case TextStyle.italic.rawValue:
+        return Text(textStyle: TextStyle.italic, string: "")
+    case TextStyle.strikethrough.rawValue:
+        return Text(textStyle: TextStyle.strikethrough, string: "")
+    default:
+        return Text(textStyle: TextStyle.regular, string: "\(character)")
     }
 }
